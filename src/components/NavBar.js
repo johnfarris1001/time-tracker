89f2a32bc9e00173d745d2e9edd528a6cfe7a70a
John <johnfarris217@gmail.com>
2023-04-06 22:07:17 -0500

    add navbar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const linkStyles = {
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "green",
+    textDecoration: "none",
+    color: "white",
+};
 
 function NavBar() {
     return (
-        <div>
-            NavBar
+        <div className='navbar'>
+            <NavLink to='/' exact style={linkStyles}>
+                Home
+            </NavLink>
+            <NavLink to='/about' exact style={linkStyles}>
+                About
+            </NavLink>
+            <NavLink to='/entrylist' exact style={linkStyles}>
+                EntryList
+            </NavLink>
+            <NavLink to='/newentry' exact style={linkStyles}>
+                NewEntry
+            </NavLink>
         </div>
     )
 }

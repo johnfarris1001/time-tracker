ab38f820774e2529e36746b9173dbf5a19d16614
John <johnfarris217@gmail.com>
2023-04-15 13:15:56 -0500

    add instructions to About page

--- src/components/About.js
+++ src/components/About.js
@@ -6,14 +6,17 @@ function About() {
         <div id='about-div'>
             <h3>Instructions:</h3>
             <ol>
-                <li></li>
+                <li>Add yourself as a user using the 'Add User' text box. Or, if you are a returning user, select your username from the 'Current User' dropdown.</li>
+                <li>Add new entries on the 'New Entry' page. Navigate to the page using the NavBar if necessary.</li>
+                <li>Add more entries for every completed task on the 'New Entry' page.</li>
+                <li>Click 'Start New Running Activity' to add an entry that you have started, but is not yet finished.</li>
+                <li>Add in the 'Activity' and the 'Type'.  Click 'End Activity' when you have finished the entry.</li>
+                <li>Navigate to the 'Entry List' page using the NavBar. All of your completed activities will be displayed here.</li>
+                <li>Use the 'Entry List' page to Delete and Edit entries.</li>
+                <li>Navigate to the 'Results' page to see a summary of how your time is spent.</li>
             </ol>
             <h4>Notes:</h4>
             <ul>
-                <li>Start by adding a user</li>
-                <li>Add new entries on the New Entry page</li>
-                <li>Start a running actvity for what you are currently working on</li>
-                <li>View how much time you spend on each activity and type on the Results page</li>
                 <li>Rounds all time to the nearest quarter hour</li>
             </ul>
         </div>
